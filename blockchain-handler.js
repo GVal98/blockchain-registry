61335ebe5d2854493d1cbc8024910e718dd25266
GVal98 <GVal98@gmail.com>
2020-05-08 14:40:29 +0300

    Validator key import

--- blockchain-handler.js
+++ blockchain-handler.js
@@ -4,7 +4,10 @@ exports.BlockchainHandler = class BlockchainHandler {
   constructor(connectionHandler) {
     this.connectionHandler = connectionHandler;
     this.setBlockchainFile();
+    this.setValidatorPrivateKeyFile();
     this.loadBlockchainFromFile();
+    this.setValidatorPrivateKey();
+    console.log(this.validatorPrivateKey);
     setInterval(() => this.updateChain(), 3500);
   }
 
@@ -21,6 +24,10 @@ exports.BlockchainHandler = class BlockchainHandler {
     [, , , , , this.blockchainFile] = process.argv;
   }
 
+  setValidatorPrivateKeyFile() {
+    [, , , , , , this.validatorPrivateKeyFile] = process.argv;
+  }
+
   loadBlockchainFromFile() {
     this.blockchain = JSON.parse(fs.readFileSync(this.blockchainFile));
   }
@@ -31,5 +38,9 @@ exports.BlockchainHandler = class BlockchainHandler {
 
   getHeight() {
     return this.blockchain.length - 1;
+  }
+
+  setValidatorPrivateKey() {
+    this.validatorPrivateKey = fs.readFileSync(this.validatorPrivateKeyFile);
   }
 };
