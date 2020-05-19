4cd0d38c67f280fa5f9bf67d7b043a815c8f3b06
GVal98 <GVal98@gmail.com>
2020-05-19 19:21:07 +0300

    Keys password encryption

--- cli/utils/keygen.js
+++ cli/utils/keygen.js
@@ -1,17 +1,31 @@
 const EdDSA = require('elliptic').eddsa;
+const CryptoJS = require("crypto-js");
 const crypto = require('crypto');
 const fs = require('fs');
+const read = require('read');
 
 class KeyGen {
   static createNewKey() {
-    const privateKeyFile = process.argv[2];
-    const eddsa = new EdDSA('ed25519');
-    const key = eddsa.keyFromSecret(crypto.randomBytes(16));
-    const publicKey = key.getPublic('hex');
-    const privateKey = key.getSecret('hex');
-    console.log(`Your public key: ${publicKey}`);
-    fs.writeFileSync(privateKeyFile, privateKey);
-    console.log(`Your private saved to ${privateKeyFile}`);
+    read({prompt: 'Enter new password:', silent: true, replace: '*'}, (error, password) => {
+      read({prompt: 'Repeat password:', silent: true, replace: '*'}, (error, password2) => {
+        if (password === password2) {
+          const privateKeyFile = process.argv[2];
+          const eddsa = new EdDSA('ed25519');
+          const key = eddsa.keyFromSecret(crypto.randomBytes(16));
+          const publicKey = key.getPublic('hex');
+          const privateKey = key.getSecret('hex');
+          console.log(`Your public key: ${publicKey}`);
+          const privateKeyEncrypted = CryptoJS.AES.encrypt(privateKey, password).toString();
+          fs.writeFileSync(privateKeyFile, privateKeyEncrypted);
+          console.log(`Your password encrypted private key saved to ${privateKeyFile}`);
+        }
+        else {
+          console.log('Error: Password missmatch');
+          KeyGen.createNewKey();
+        }
+      })
+    })
+    
   }
 }
 
