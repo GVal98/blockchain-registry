93c3653bb66598c3384df295b50b5075604114f7
GVal98 <GVal98@gmail.com>
2020-05-30 00:58:38 +0300

    build fixed

--- gui/app.js
+++ gui/app.js
@@ -6,6 +6,7 @@ const { BlockchainHandler } = require('./blockchain-handler');
 const { Server } = require('./server');
 const { ElectronApp } = require('./gui/electron-app');
 const { ipcMain } = require('electron');
+const fs = require('fs');
 
 class App {
   async run() {
@@ -15,12 +16,27 @@ class App {
     // console.log('Electron app started');
     this.electronApp = electronApp.getApp();
     this.appPath = this.electronApp.getAppPath();
+    this.userPath = this.electronApp.getPath('userData');
     this.elliptic = new Elliptic();
     this.transactionHelper = new TransactionHelper(this.elliptic);
     this.blockHelper = new BlockHelper(this.elliptic, this.transactionHelper);
 
-    this.connectionHandler = new ConnectionHandler(this.transactionHelper, `${this.appPath}/nodes.json`, electronApp);
-    this.blockchainHandler = new BlockchainHandler(this.transactionHelper, this.blockHelper, `${this.appPath}/blockchain.json`, electronApp);
+    this.defaultNodesPath = `${this.appPath}/nodes.json`;
+    this.defaultBlockchainPath = `${this.appPath}/blockchain.json`;
+    this.userNodesPath = `${this.userPath}/nodes.json`;
+    this.userBlockchainPath = `${this.userPath}/blockchain.json`;
+
+    if (!fs.existsSync(this.userNodesPath)) {
+      fs.writeFileSync(this.userNodesPath, fs.readFileSync(this.defaultNodesPath));
+      console.log(`${this.userNodesPath} created`);
+    }
+    if (!fs.existsSync(this.userBlockchainPath)) {
+      fs.writeFileSync(this.userBlockchainPath, fs.readFileSync(this.defaultBlockchainPath));
+      console.log(`${this.userBlockchainPath} created`);
+    }
+    
+    this.connectionHandler = new ConnectionHandler(this.transactionHelper, this.userNodesPath, electronApp);
+    this.blockchainHandler = new BlockchainHandler(this.transactionHelper, this.blockHelper, this.userBlockchainPath, electronApp);
 
     this.connectionHandler.setBlockchainHandler(this.blockchainHandler);
     this.blockchainHandler.setConnectionHandler(this.connectionHandler);
